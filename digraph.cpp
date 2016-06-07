c407da7d667fe0a7670a070fe5fb5c3d2c0c25b9
kingpin <saurabhsshukla@gmail.com>
2016-06-07 15:47:39 +0530

    digraph: BFS complete. Input improved. File based input

--- digraph.cpp
+++ digraph.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
+#include<fstream>
 #include<cstdlib>
 #include<cstring>
+#include<queue>
 #include<limits>
 
 #define LONGMAX std::numeric_limits<long long>::max()
@@ -81,6 +83,28 @@ public:
         E++;
     }
 
+    bool BFS(int src, int dest) {
+        queue<int> ndQueue;
+        bool found = false;
+        AdjNode *nd = NULL;
+        ndQueue.push(src);
+        double spath_wt = 0, this_wt=0;
+        char *visited = new char(this->V);
+        while(!ndQueue.empty()) {
+            int indx = ndQueue.front();
+            ndQueue.pop();
+            nd = alist[indx].head;
+            if(nd) visited[nd->vtx] = 1;
+            while(nd) {
+                if(nd->vtx == dest) found = true;
+                if(!visited[nd->vtx])
+                    ndQueue.push(nd->vtx);
+                nd=nd->next;
+            }
+        }
+        return found;
+    }
+
     void print() {
         cout<<"Total edges in graph:"<<E<<endl;
         cout<<"Adjecency lists for ";
@@ -98,32 +122,51 @@ public:
     }
 };
 
-void create_graph(Graph &G) {
+void create_graph_from_file(Graph &G, ifstream &ifs ) {
 
     int v1,v2;
     double wt;
-    cout<<"Enter edges in format \"v1 v2 <weight(can be fractional value)>\"(ctrl+D to finish):"<<endl;
-    while(true) {
-        cin>>v1>>v2>>wt;
-        if(cin.eof()) {
-            cin.clear();
-            break;
-        }
+    int counter=0;
+    ifs>>counter;
+    while(ifs.good() && counter) {
+        ifs>>v1>>v2>>wt;
+        --counter;
         if(v1>=G.V || v2>=G.V || v1==v2) {
             cout<<"v1 and v2 should not be same and in limits of 0-"<<G.V-1<<endl;
             continue;
         }
+        else cout<<v1<<" "<<v2<<" "<<wt<<endl;
         G.add_edge(v1,v2,wt);
     }
 }
 
 int main() {
-    int vcount;
-    cout<<"Enter number of vertices:";
-    cin>>vcount;
+    int vcount, v1, v2;
+    char fname[1024] = {0};
+
+    cout<<"Enter input file name:";
+    //Input file should be of the below format
+    /*
+    m<vertices>
+    n<edges>
+    <start> <end> weight
+    :(n rows )
+    <start v> <end v> for BFS
+    */
+    cin>>fname;
+    std::ifstream ifs(fname, ifstream::in);
+
+    ifs>>vcount;
     Graph g(vcount);
-    create_graph(g);
+    create_graph_from_file(g, ifs);
     g.print();
+
+    ifs>>v1>>v2;
+    cout<<"start and end vertices for BFS:"<<v1<<" "<<v2<<endl;
+    if(g.BFS(v1, v2))
+        cout<<v1<<" and "<<v2<<" are connected"<<endl;
+    else
+        cout<<v1<<" and "<<v2<<" are not connected"<<endl;
     return 0;
 }
 
